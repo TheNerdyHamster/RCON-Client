35c238f72f2bda6418bc4dab67b381ef9e8681cd
Lilahamstern <leo.ronnebro@hotmail.com>
2020-03-13 16:11:55 +0100

    Init commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+  Button,
+  Classes,
+  Navbar,
+  NavbarDivider,
+  NavbarGroup,
+  Popover,
+  Menu,
+  MenuItem,
+  Divider
+} from "@blueprintjs/core";
+
+import { remote } from "electron";
+
+const showMessageBox = remote.dialog.showMessageBox;
+const app = remote.app;
+
+export default class NavbarComponent extends React.Component {
+  render() {
+    return (
+      <Navbar>
+        <NavbarGroup>
+          <Popover
+            content={
+              <Menu>
+                <MenuItem
+                  text="Add new connection"
+                  onClick={() =>
+                    showMessageBox({
+                      type: "info",
+                      message: "Activated",
+                      button: ["close"]
+                    })
+                  }
+                />
+                <Divider></Divider>
+                <MenuItem
+                  text="Connection list"
+                  onClick={() =>
+                    showMessageBox({
+                      type: "info",
+                      message: "Activated",
+                      button: ["close"]
+                    })
+                  }
+                />
+              </Menu>
+            }
+          >
+            <Button className={Classes.MINIMAL} icon="plus"></Button>
+          </Popover>
+        </NavbarGroup>
+      </Navbar>
+    );
+  }
+}
---
+++ src/windows/main/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../../components/navbar";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div
+        id="layout"
+        style={{ display: "flex", flexDirection: "column", height: "100vh" }}
+      >
+        <Navbar></Navbar>
+      </div>
+    );
+  }
+}
